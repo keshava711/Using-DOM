99caa5d537c6c03aa8ca10362bdb4305e626a33f
KESHAVA KRISHNA <keshavakrishna72@gmail.com>
2024-11-06 11:27:48 +0530

    files are added

---
+++ dom.js
@@ -0,0 +1,10 @@
+var a=document.getElementById("btn")
+function b(){
+    a.textContent="hello! Every One"
+    a.style.fontSize="60px"
+    a.style.backgroundColor="red"
+    a.style.borderradius="50%"
+    console.log(a)
+    a.innerHTML='hello! Every One<img src="https://img.freepik.com/free-photo/abstract-autumn-beauty-multi-colored-leaf-vein-pattern-generated-by-ai_188544-9871.jpg" width="200px" Style="border-radius:30px;"/>     <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_640.jpg" width="200px" Style="border-radius:100px;"/>  <img src="https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-autumn-nature-with-trees-of-yellow-leaves-free-image.jpeg?w=600&quality=80" width="200px" Style="border-radius:50%;"/>'
+}
+
